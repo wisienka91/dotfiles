629849386f9893ee137ef9b41c54a93ee39ccbc3
wisienka91 <wisnia91@gmail.com>
2017-06-05 14:37:32 +0200

    Fix zsh_install for ubuntu 16.04

--- zsh_install.sh
+++ zsh_install.sh
@@ -2,10 +2,10 @@
 
 #zsh install
 echo 'Installing zsh...'
-sudo apt-get install zsh
+sudo apt-get install zsh -y
 
 echo 'Installing git-core...'
-sudo apt-get install git-core
+sudo apt-get install git-core -y
 
 echo 'Installing oh-my-zsh...'
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
